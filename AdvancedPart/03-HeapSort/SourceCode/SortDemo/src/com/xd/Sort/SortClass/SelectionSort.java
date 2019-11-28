210d948bb705b752d52a04d319919b8091773bdf
MRT <755737558@qq.com>
2019-11-28 09:13:47 +0800

    Create SelectionSort.java

---
+++ AdvancedPart/03-HeapSort/SourceCode/SortDemo/src/com/xd/Sort/SortClass/SelectionSort.java
@@ -0,0 +1,16 @@
+package com.xd.Sort.SortClass;
+
+public class SelectionSort extends Sort {
+    @Override
+    protected void sort() {
+        for (int end = array.length - 1; end > 0; end--) {
+            int maxIndex = 0;
+            for (int begin = 1; begin <= end ; begin++) {
+                if (cmp(maxIndex,begin) <= 0) {
+                    maxIndex = begin;
+                }
+            }
+            swap(maxIndex,end);
+        }
+    }
+}
