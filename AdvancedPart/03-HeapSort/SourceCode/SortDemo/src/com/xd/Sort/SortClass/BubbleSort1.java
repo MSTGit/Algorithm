463b7c76e0ed88714e473da40e1b107fc18d0925
MRT <755737558@qq.com>
2019-11-28 09:13:38 +0800

    Create BubbleSort1.java

---
+++ AdvancedPart/03-HeapSort/SourceCode/SortDemo/src/com/xd/Sort/SortClass/BubbleSort1.java
@@ -0,0 +1,14 @@
+package com.xd.Sort.SortClass;
+
+public class BubbleSort1 extends Sort {
+    @Override
+    protected void sort() {
+        for (int end = array.length - 1; end > 0; end--) {
+            for (int begin = 1; begin <= end ; begin++) {
+                if (cmp(begin,begin - 1) < 0) {
+                    swap(begin,begin - 1);
+                }
+            }
+        }
+    }
+}
