e734688f137480de99d6d87b2cbf693792dfd803
MRT <755737558@qq.com>
2019-11-28 09:13:43 +0800

    Create BubbleSort3.java

---
+++ AdvancedPart/03-HeapSort/SourceCode/SortDemo/src/com/xd/Sort/SortClass/BubbleSort3.java
@@ -0,0 +1,18 @@
+package com.xd.Sort.SortClass;
+
+public class BubbleSort3 extends Sort{
+
+    @Override
+    protected void sort() {
+        for (int end = array.length - 1; end > 0; end--) {
+            int sortedIndex = 1;//该值在数组完全逆序许的时候有用
+            for (int begin = 1; begin <= end ; begin++) {
+                if (cmp(begin,begin - 1) < 0) {
+                   swap(begin,begin - 1);
+                    sortedIndex = begin;
+                }
+            }
+            end = sortedIndex;
+        }
+    }
+}
