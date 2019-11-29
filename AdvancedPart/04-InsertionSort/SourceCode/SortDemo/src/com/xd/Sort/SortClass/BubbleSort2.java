48931f863d57dd93d8cd1628a99abb3266ba7759
MRT <755737558@qq.com>
2019-11-29 22:17:38 +0800

    Create BubbleSort2.java

---
+++ AdvancedPart/04-InsertionSort/SourceCode/SortDemo/src/com/xd/Sort/SortClass/BubbleSort2.java
@@ -0,0 +1,19 @@
+package com.xd.Sort.SortClass;
+
+public class BubbleSort2<E  extends Comparable<E>> extends Sort {
+    @Override
+    protected void sort() {
+        for (int end = array.length - 1; end > 0; end--) {
+            boolean sorted = true;
+            for (int begin = 1; begin <= end ; begin++) {
+                if (cmp(begin,begin - 1) < 0) {
+                    swap(begin,begin - 1);
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+    }
+}
